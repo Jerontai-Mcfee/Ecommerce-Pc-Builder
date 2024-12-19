81ea997474e9da9832fa77b2499f13f1dac9129d
Jerontai-Mcfee <jerontai07@gmail.com>
2024-12-18 20:37:56 -0500

    Enables server app

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const mongoose = requre('mongoose');
+const mongoose = require('mongoose');
 const cookieparser = require('cookie-parser');
-const cors =requre('cors');
+const cors = require('cors');
 
 
 // create a database connection
-//create a seperate file for this and then import / use that file 
+// create a seperate file for this and then import / use that file 
 
 mongoose.connect("mongodb+srv://jerontai07:Hopeisthere@28@cluster0.a74yt.mongodb.net/"
 )
@@ -18,5 +18,21 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(
-    cors({})
-)
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            "Content -Type",
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials : true
+    })
+);
+
+app.use(cookieparser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
